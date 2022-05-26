458acc800a8f92a33fe34fde66456dcbe871c2dc
Minh Tran <minh.tran@asnet.com.vn>
2022-05-26 14:46:08 +0700

    Update script to test coverage result

--- bin/dj-test.sh
+++ bin/dj-test.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
 
-if [ -z "$TEST_APP" ]; then
-    coverage run --source='.' src/manage.py test src -v 2 && coverage html
-else
-    coverage erase && coverage run --source='.' src/manage.py test "src/${TEST_APP}/tests/" -v 2 && coverage html
-fi
+MIN_COVERAGE=$1
+
+# if [ -z "$TEST_APP" ]; then
+#     coverage run --source='.' src/manage.py test src -v 2 && coverage html
+# else
+#     coverage erase && coverage run --source='.' src/manage.py test "src/${TEST_APP}/tests/" -v 2 && coverage html
+# fi
+
+coverage erase && coverage run --source='.' src/manage.py test "src/${TEST_APP}/tests/" -v 2 && coverage html
 
 coverage report -m
 
 COVERAGE_RESULT=`coverage report | grep TOTAL | awk 'N=1 {print $NF}' | sed 's/%//g'`
 
-echo ::set-output name=coverage_result::$COVERAGE_RESULT
+# echo ::set-output name=coverage_result::$COVERAGE_RESULT
+
+
+if [[ $COVERAGE_RESULT -gt $MIN_COVERAGE ]]; then
+    echo ::set-output name=coverage_result::$COVERAGE_RESULT
+else
+    echo "#################################################"
+    echo "Code coverage below allowed threshold ($COVERAGE_RESULT<$MIN_COVERAGE)"
+    exit 1
+fi
+
+echo "#################################################"
+echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
