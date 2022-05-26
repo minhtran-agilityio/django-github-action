193675ee7d9739209c0a2fd4ed2a51fe7ae142c9
Minh Tran <minh.tran@asnet.com.vn>
2022-05-26 16:52:49 +0700

    Clean code

--- bin/dj-test.sh
+++ bin/dj-test.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
+# Set default min coverage percentage if not exist
 if [ -z "$MIN_COVERAGE" ]; then
     MIN_COVERAGE=80
 fi
-
-echo "---> TARGET COVERAGE: ${MIN_COVERAGE}%"
 
 coverage run --source='.' src/manage.py test src -v 2
 coverage report -m
 
 COVERAGE_RESULT=`coverage report | grep TOTAL | awk 'N=1 {print $NF}' | sed 's/%//g'`
-
-# echo ::set-output name=coverage_result::$COVERAGE_RESULT
-
 
 if [[ $COVERAGE_RESULT -gt $MIN_COVERAGE ]]; then
     echo "--------------------------------------------------------------------"
