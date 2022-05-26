d2f1385bdee5d1f77f223e7509e4439ce9de39fa
Minh Tran <minh.tran@asnet.com.vn>
2022-05-26 16:43:04 +0700

    Update for run test cov

--- bin/dj-test.sh
+++ bin/dj-test.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
-MIN_COVERAGE=200
-
-# if [ -z "$TEST_APP" ]; then
-#     coverage run --source='.' src/manage.py test src -v 2 && coverage html
-# else
-#     coverage erase && coverage run --source='.' src/manage.py test "src/${TEST_APP}/tests/" -v 2 && coverage html
-# fi
+if [ -z "$MIN_COVERAGE" ]; then
+    MIN_COVERAGE=80
+fi
 
 coverage run --source='.' src/manage.py test src -v 2
-
 coverage report -m
 
 COVERAGE_RESULT=`coverage report | grep TOTAL | awk 'N=1 {print $NF}' | sed 's/%//g'`
@@ -18,12 +13,14 @@ COVERAGE_RESULT=`coverage report | grep TOTAL | awk 'N=1 {print $NF}' | sed 's/%
 
 
 if [[ $COVERAGE_RESULT -gt $MIN_COVERAGE ]]; then
+    echo "--------------------------------------------------------------------"
     echo ::set-output name=coverage_result::$COVERAGE_RESULT
+    echo "--------------------------------------------------------------------"
 else
-    echo "#################################################"
+    echo "--------------------------------------------------------------------"
     echo "Code coverage below allowed threshold ($COVERAGE_RESULT<$MIN_COVERAGE)"
+    echo "--------------------------------------------------------------------"
     exit 1
 fi
 
-echo "#################################################"
 echo "Completed!"
